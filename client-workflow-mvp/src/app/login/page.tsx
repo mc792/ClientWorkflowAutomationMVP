9b493fada7064c4955921807e88b4ee8deb2a158
mc792 <curmatt02@yahoo.it>
2026-02-23 10:21:21 +0100

    trigger redeploy

--- client-workflow-mvp/src/app/login/page.tsx
+++ client-workflow-mvp/src/app/login/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import { useRouter } from "next/navigation";
 
+
 export default function LoginPage() {
   const router = useRouter();
   const [email, setEmail] = useState("");
